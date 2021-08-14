4ad97df0ccd3ce3037da7effb60f82eaeb41f53c
Brody Dingel <brodydingel795@gmail.com>
2021-08-13 20:37:31 -0500

    ITS WORKING WOOOO

--- __TEST__/login.spec.js
+++ __TEST__/login.spec.js
@@ -5,14 +5,19 @@ import "@testing-library/jest-dom/extend-expect";
 import user from "@testing-library/user-event";
 import { renderWithProviders, server } from "./helpers";
 import { ApolloProvider } from "@apollo/client";
-import { client } from '../components/apollo-client';
+import { client } from "../components/apollo-client";
 
 describe("login", () => {
-	const renderComponent = () => render(<ApolloProvider client={client}>
-		<LoginForm />
-	</ApolloProvider>);
+	const renderComponent = () =>
+		render(
+			<ApolloProvider client={client}>
+				<LoginForm />
+			</ApolloProvider>
+		);
 
-	beforeAll(() => server.listen);
+	beforeAll(() => {
+		server.listen({ onUnhandledRequest: "error" });
+	});
 
 	beforeEach(() => {
 		server.resetHandlers();
@@ -26,25 +31,6 @@ describe("login", () => {
 
 		user.type(await screen.findByLabelText("Username:"), "brody");
 		user.click(await screen.findByText("Submit"));
-
-		/*
-		So far, so good! The console logs show that the form is successfully submitted with the expected input,
-		but as soon as it gets to the GraphQL mutation, it can't find a matching operation in `handlers.js`
-		and it crashes with:
-
-			error: ApolloError: No more mocked responses for the query: mutation Login($username: String!) {
-				user {
-					id
-					firstName
-					lastName
-					__typename
-				}
-			}
-			
-			Expected variables: {"username":"brody"}
-		
-		In fact, it seems that it never even hits the mutation...? See `handlers.js`
-		*/
 
 		expect(await screen.findByText("Maverick")).toBeInTheDocument();
 	});
