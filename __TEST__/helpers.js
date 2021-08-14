aef5fa7d0aa9c1cd4ae21053149f55ee03770df2
Brody Dingel <brodydingel795@gmail.com>
2021-08-13 20:43:46 -0500

    Move render function to helpers

--- __TEST__/helpers.js
+++ __TEST__/helpers.js
@@ -1,4 +1,4 @@
-import { MockedProvider } from "@apollo/client/testing";
+import { ApolloProvider } from "@apollo/client";
 import { render } from "@testing-library/react";
 import { setupServer } from "msw/node";
 import { handlers } from "../__MOCKS__/handlers";
@@ -9,5 +9,7 @@ export const server = setupServer(...handlers);
 export const renderWithProviders = (ui) => {
 	const Wrapper = () => <ApolloProvider client={client}>{ui}</ApolloProvider>;
 
+	// Note: KCD recommended a different form of doing this.
+	// watch office hours video to implement that
 	return render(<Wrapper />);
 };
--- __TEST__/login.spec.js
+++ __TEST__/login.spec.js
@@ -1,20 +1,12 @@
-import { LoginForm } from "../components/login-form";
-
-import { screen, render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import user from "@testing-library/user-event";
+
+import { LoginForm } from "../components/login-form";
+
 import { renderWithProviders, server } from "./helpers";
-import { ApolloProvider } from "@apollo/client";
-import { client } from "../components/apollo-client";
 
 describe("login", () => {
-	// const renderComponent = () =>
-	// 	render(
-	// 		<ApolloProvider client={client}>
-	// 			<LoginForm />
-	// 		</ApolloProvider>
-	// 	);
-
 	// for this server.listen to work, this beforeAll HAS to have a full block,
 	// unlike where we can have `beforeAll(() => RTL.cleanup)` and that works fine
 	beforeAll(() => {
